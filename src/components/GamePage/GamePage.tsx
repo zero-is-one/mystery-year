f9e24637a37f3f9746567d71ff0f2dae6a73037f
Luke NB <luke@0is1.com>
2023-02-04 14:18:06 -0500

    game page animations,scopes, top cards,

--- src/components/GamePage/GamePage.tsx
+++ src/components/GamePage/GamePage.tsx
@@ -6,35 +6,57 @@ import testImg from "assets/images/test-image.jpg";
 import testImg2 from "assets/images/test-image2.jpg";
 import AnimatedNumber from "react-awesome-animated-number";
 import { BsArrowRightShort } from "react-icons/bs";
+import { GiRoundStar } from "react-icons/gi";
 import { animate, motion, AnimatePresence } from "framer-motion";
 import { Range } from "./Range";
 import { CaretSelector } from "./CaretSelector";
+import { Scope } from "./Scope";
+import { useSingleImageGameData } from "hooks/useSingleImageGameData/useSingleImageGameData";
 
 export const GamePage = () => {
-  const [year, setYear] = useState<number>(1950);
-  const [helpText, setHelpText] = useState<string>(
-    "When was this photo taken?"
-  );
+  const {
+    makeGuess,
+    minYear,
+    maxYear,
+    points,
+    guessYear,
+    targetYear,
+    hasMadeGuess,
+    color: guessPointColor,
+  } = useSingleImageGameData(2000);
+
+  const [totalPoints, setTotalPoints] = useState(0);
+  const [rangeVal, setRangeVal] = useState<number>(1950);
+
   const [imageUrl, setImageUrl] = useState<string>(testImg);
 
   const submitGuess = () => {
-    console.log("submit guess");
-    setHelpText("This photo was taken in");
-    animate(year, 2000, {
+    const userGuess = rangeVal;
+    animate(userGuess, targetYear, {
       duration: 0.2,
-      onUpdate: (latest) => setYear(Math.floor(latest)),
+      onUpdate: (latest) => setRangeVal(Math.floor(latest)),
     });
+    const points = makeGuess(userGuess);
+    setTotalPoints(totalPoints + points);
   };
 
   return (
     <>
       <main>
         <div className="container">
-          <div className="row">
-            <div className="col">
-              <Viewer imageUrl={imageUrl} />
+          <div className="" style={{ position: "relative" }}>
+            <div className="game-info-card" style={{ right: 0, left: "auto" }}>
+              <small>Points</small>
+
+              <AnimatedNumber value={totalPoints} duration={100} size={16} />
+            </div>
+            <div className="game-info-card">
+              <small>Round</small>
+              <p className="m-0">1 / 5</p>
             </div>
           </div>
+
+          <Viewer imageUrl={imageUrl} />
 
           <div className="d-flex justify-content-end align-items-center">
             <div>
@@ -44,26 +66,79 @@ export const GamePage = () => {
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
                   className="me-4"
-                  key={helpText}
                   transition={{ duration: 0.3 }}
+                  key={hasMadeGuess ? "yes" : "no"}
                 >
-                  {helpText}
+                  {hasMadeGuess
+                    ? `This photo was taken on:`
+                    : `When was this photo taken?`}
                 </motion.p>
               </AnimatePresence>
             </div>
             <div>
               <AnimatedNumber
-                value={year}
+                className="game-large-number"
+                value={rangeVal}
                 duration={100}
                 hasComma={false}
                 size={96}
               />
             </div>
             <div style={{ width: "33%" }}>
-              <CaretSelector value={year} setValue={setYear} />
+              <AnimatePresence mode="wait">
+                {!hasMadeGuess && (
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="me-4"
+                    transition={{ duration: 0.3 }}
+                  >
+                    <CaretSelector value={rangeVal} setValue={setRangeVal} />
+                  </motion.div>
+                )}
+
+                {hasMadeGuess && (
+                  <AnimatePresence mode="wait">
+                    <motion.div
+                      initial={{ opacity: 0, y: 20 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0 }}
+                      className="ms-4"
+                      transition={{ duration: 0.3 }}
+                    >
+                      {targetYear === guessYear && (
+                        <GiRoundStar
+                          color="#F2BB29"
+                          size={30}
+                          className="spin"
+                        />
+                      )}
+                      <span style={{ color: guessPointColor, fontSize: 22 }}>
+                        +{points}{" "}
+                      </span>
+                      <small>points</small>
+                    </motion.div>
+                  </AnimatePresence>
+                )}
+              </AnimatePresence>
             </div>
           </div>
-          <Range value={year} setValue={setYear} />
+          <Range
+            value={rangeVal}
+            setValue={setRangeVal}
+            minValue={minYear}
+            maxValue={maxYear}
+            disabled={hasMadeGuess}
+          />
+          <Scope
+            minValue={minYear}
+            maxValue={maxYear}
+            range={[guessYear, targetYear]}
+            rangeColor={guessPointColor}
+            rangeLabel={`${guessYear}`}
+          />
+
           <div className="d-flex align-items-center justify-content-center">
             <button
               className="btn me-4 my-2 btn-lg btn-primary d-flex align-items-center justify-content-center"
---
+++ src/components/GamePage/Scope.tsx
@@ -0,0 +1,90 @@
+import { motion } from "framer-motion";
+
+export const Scope = ({
+  minValue,
+  maxValue,
+  range,
+  rangeColor,
+  rangeLabel,
+}: {
+  minValue: number;
+  maxValue: number;
+  range: [number | undefined, number | undefined];
+  rangeColor: string;
+  rangeLabel?: string;
+}) => {
+  const interval = window.innerWidth < 700 ? 20 : 10;
+
+  return (
+    <div style={{ position: "relative", userSelect: "none", height: 60 }}>
+      {[...Array(Math.floor((maxValue - minValue) / interval))].map((_, i) => {
+        const val = minValue + i * interval;
+
+        return (
+          <div
+            key={i}
+            style={{
+              position: "absolute",
+              left: getPosition(val, minValue, maxValue) + "%",
+              opacity: 0.18,
+              borderLeft: "1px solid #BCBCBC",
+              paddingLeft: 5,
+              paddingTop: 20,
+            }}
+          >
+            {val}
+          </div>
+        );
+      })}
+
+      {range[0] && range[1] && range[0] !== range[1] && (
+        <motion.div
+          style={{
+            background: `linear-gradient(to right, ${rangeColor.replace(
+              "100%,50%)",
+              "100%,30%)"
+            )} , ${rangeColor})`,
+            position: "absolute",
+            zIndex: -1,
+            height: 12,
+            transform: range[0] > range[1] ? "scaleX(-1)" : "",
+            transformOrigin: "left",
+            top: 4,
+            left: getPosition(range[0], minValue, maxValue) + "%",
+            fontSize: 10,
+            display: "flex",
+            alignItems: "center",
+
+            paddingLeft: 10,
+          }}
+          transition={{ duration: 1 }}
+          initial={{ width: 0 }}
+          animate={{
+            width:
+              Math.abs(
+                getPosition(range[1], minValue, maxValue) -
+                  getPosition(range[0], minValue, maxValue)
+              ) + "%",
+          }}
+        >
+          {rangeLabel && (
+            <span
+              style={{
+                transform:
+                  range[0] > range[1]
+                    ? `scaleX(-1) translateX(calc(100% + 15px))`
+                    : "translateX(calc(-100% - 15px))",
+              }}
+            >
+              {rangeLabel}
+            </span>
+          )}
+        </motion.div>
+      )}
+    </div>
+  );
+};
+
+const getPosition = (value: number, minValue: number, maxValue: number) => {
+  return ((value - minValue) / (maxValue - minValue)) * 100;
+};
